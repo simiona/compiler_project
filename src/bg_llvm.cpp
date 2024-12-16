760b5329c16a7d1edf2758e70906232a2667bb8c
ChenDongqi <2091984032@qq.com>
2024-12-16 08:09:55 +0000

    start of ass6

--- src/bg_llvm.cpp
+++ src/bg_llvm.cpp
@@ -13,20 +13,25 @@ using namespace std;
 using namespace LLVMIR;
 using namespace GRAPH;
 
-static Graph<L_block*> RA_bg;
-static unordered_map<Temp_label*, L_block*> block_env;
+static Graph<L_block *> RA_bg;
+static unordered_map<Temp_label *, L_block *> block_env;
 
-Graph<L_block*>& Bg_graph() {
+Graph<L_block *> &Bg_graph()
+{
     return RA_bg;
 }
-unordered_map<Temp_label*, L_block*>& Bg_block_env() {
+unordered_map<Temp_label *, L_block *> &Bg_block_env()
+{
     return block_env;
 }
 
-Node<L_block*>* Look_bg(L_block* b) {
-    Node<L_block*>* n1 = nullptr;
-    for (auto n : *RA_bg.nodes()) {
-        if (n.second->nodeInfo() == b) {
+Node<L_block *> *Look_bg(L_block *b)
+{
+    Node<L_block *> *n1 = nullptr;
+    for (auto n : *RA_bg.nodes())
+    {
+        if (n.second->nodeInfo() == b)
+        {
             n1 = n.second;
             break;
         }
@@ -37,9 +42,10 @@ Node<L_block*>* Look_bg(L_block* b) {
         return n1;
 }
 
-static void Enter_bg(L_block* b1, L_block* b2) {
-    Node<L_block*>* n1 = Look_bg(b1);
-    Node<L_block*>* n2 = Look_bg(b2);
+static void Enter_bg(L_block *b1, L_block *b2)
+{
+    Node<L_block *> *n1 = Look_bg(b1);
+    Node<L_block *> *n2 = Look_bg(b2);
     RA_bg.addEdge(n1, n2);
     return;
 }
@@ -47,18 +53,22 @@ static void Enter_bg(L_block* b1, L_block* b2) {
 /* input LLVMIR::L_block* *List after instruction selection for each block,
     generate a graph on the basic blocks */
 
-Graph<L_block*>& Create_bg(list<L_block*>& bl) {
-    RA_bg = Graph<L_block*>();
-    block_env = unordered_map<Temp_label*, L_block*>();
+Graph<L_block *> &Create_bg(list<L_block *> &bl)
+{
+    RA_bg = Graph<L_block *>();
+    block_env = unordered_map<Temp_label *, L_block *>();
 
-    for (auto block : bl) {
+    for (auto block : bl)
+    {
         block_env.insert({block->label, block});
         RA_bg.addNode(block);
     }
 
-    for (auto block : bl) {
-        unordered_set<Temp_label*> succs = block->succs;
-        for (auto label : succs) {
+    for (auto block : bl)
+    {
+        unordered_set<Temp_label *> succs = block->succs;
+        for (auto label : succs)
+        {
             Enter_bg(block, block_env[label]);
         }
     }
@@ -66,27 +76,51 @@ Graph<L_block*>& Create_bg(list<L_block*>& bl) {
 }
 
 // maybe useful
-static void DFS(Node<L_block*>* r, Graph<L_block*>& bg) {
+static void DFS(Node<L_block *> *r, Graph<L_block *> &bg)
+{
+    r->color = 1; // Mark the node as visited
 
+    // Recursively visit all unvisited successor nodes
+    for (auto succ_num : *r->succ())
+    {
+        auto succ_node = bg.mynodes[succ_num];
+        if (succ_node->color != 1)
+            DFS(succ_node, bg);
+    }
 }
 
-void SingleSourceGraph(Node<L_block*>* r, Graph<L_block*>& bg,L_func*fun) {
-    //   Todo
+void SingleSourceGraph(Node<L_block *> *r, Graph<L_block *> &bg, L_func *fun)
+{
+    DFS(r, bg);
+
+    int num = 0;
+    for (auto block : fun->blocks)
+    {
+        if (bg.mynodes[num]->color != 1)
+        {
+            fun->blocks.remove(block);
+        }
+        num++;
+    }
 }
 
-void Show_graph(FILE* out,GRAPH::Graph<LLVMIR::L_block*>&bg){
-    for(auto block_node:bg.mynodes){
-        auto block=block_node.second->info;
-        fprintf(out,"%s \n",block->label->name.c_str());
-        fprintf(out,"pred  %zu  ",block_node.second->preds.size());
-        for(auto pred:block_node.second->preds){
-            fprintf(out,"%s  ",bg.mynodes[pred]->info->label->name.c_str());
+void Show_graph(FILE *out, GRAPH::Graph<LLVMIR::L_block *> &bg)
+{
+    for (auto block_node : bg.mynodes)
+    {
+        auto block = block_node.second->info;
+        fprintf(out, "%s \n", block->label->name.c_str());
+        fprintf(out, "pred  %zu  ", block_node.second->preds.size());
+        for (auto pred : block_node.second->preds)
+        {
+            fprintf(out, "%s  ", bg.mynodes[pred]->info->label->name.c_str());
         }
-        fprintf(out,"\n");
-        fprintf(out,"succ  %zu  ",block_node.second->succs.size());
-        for(auto succ:block_node.second->succs){
-            fprintf(out,"%s  ",bg.mynodes[succ]->info->label->name.c_str());
+        fprintf(out, "\n");
+        fprintf(out, "succ  %zu  ", block_node.second->succs.size());
+        for (auto succ : block_node.second->succs)
+        {
+            fprintf(out, "%s  ", bg.mynodes[succ]->info->label->name.c_str());
         }
-        fprintf(out,"\n");
+        fprintf(out, "\n");
     }
 }
--- src/graph.hpp
+++ src/graph.hpp
@@ -7,139 +7,156 @@
 #include <unordered_map>
 #include <vector>
 
-namespace GRAPH {
+namespace GRAPH
+{
 
-template <typename T>
-class Graph;
-template <typename T>
-struct Node;
-typedef std::set<int> NodeSet;
+    template <typename T>
+    class Graph;
+    template <typename T>
+    struct Node;
+    typedef std::set<int> NodeSet;
 
-template <typename T>
-struct Node {
-    int mykey;
-    Graph<T>* mygraph;
-    NodeSet succs;
-    NodeSet preds;
-    T info;
-    int color;
+    template <typename T>
+    struct Node
+    {
+        int mykey;
+        Graph<T> *mygraph;
+        NodeSet succs;
+        NodeSet preds;
+        T info;
+        int color;
 
-    Node() {}
-    Node(int _mykey, Graph<T>* _mygraph, T _info, int _color)
-        : mykey(_mykey), mygraph(_mygraph), info(_info), color(_color) {
-        succs = NodeSet();
-        preds = NodeSet();
+        Node() {}
+        Node(int _mykey, Graph<T> *_mygraph, T _info, int _color)
+            : mykey(_mykey), mygraph(_mygraph), info(_info), color(_color)
+        {
+            succs = NodeSet();
+            preds = NodeSet();
+        }
+
+        T nodeInfo();
+        NodeSet *succ();
+        NodeSet *pred();
+        int nodeid();
+        int outDegree();
+        int inDegree();
+    };
+
+    template <typename T>
+    class Graph
+    {
+    public:
+        std::map<int, Node<T> *> mynodes;
+        int nodecount;
+        Graph()
+        {
+            nodecount = 0;
+            mynodes = std::map<int, Node<T> *>();
+        }
+        /* Make a new node in graph "g", with associated "info" */
+        Node<T> *addNode(T info);
+
+        void rmNode(Node<T> *);
+
+        /* Get the list of nodes belonging to "g" */
+        std::map<int, Node<T> *> *nodes();
+
+        /* Make a new edge joining nodes "from" and "to", which must belong
+            to the same graph */
+        void addEdge(Node<T> *from, Node<T> *to);
+
+        /* Delete the edge joining "from" and "to" */
+        void rmEdge(Node<T> *from, Node<T> *to);
+
+        /* Tell if there is an edge from "from" to "to" */
+        bool goesTo(Node<T> *from, Node<T> *n);
+    };
+
+    template <typename T>
+    T Node<T>::nodeInfo()
+    {
+        return this->info;
     }
 
-    T nodeInfo();
-    NodeSet* succ();
-    NodeSet* pred();
-    int nodeid();
-    int outDegree();
-    int inDegree();
-};
-
-template <typename T>
-class Graph {
-   public:
-    std::map<int, Node<T>*> mynodes;
-    int nodecount;
-    Graph() {
-        nodecount = 0;
-        mynodes = std::map<int, Node<T>*>();
+    template <typename T>
+    NodeSet *Node<T>::succ()
+    {
+        return &this->succs;
     }
-    /* Make a new node in graph "g", with associated "info" */
-    Node<T>* addNode(T info);
 
-    void rmNode(Node<T>*);
+    template <typename T>
+    NodeSet *Node<T>::pred()
+    {
+        return &this->preds;
+    }
 
-    /* Get the list of nodes belonging to "g" */
-    std::map<int, Node<T>*>* nodes();
+    template <typename T>
+    int Node<T>::nodeid()
+    {
+        return this->mykey;
+    }
 
-    /* Make a new edge joining nodes "from" and "to", which must belong
-        to the same graph */
-    void addEdge(Node<T>* from, Node<T>* to);
+    template <typename T>
+    int Node<T>::inDegree()
+    {
+        int deg = 0;
+        return this->preds.size();
+    }
 
-    /* Delete the edge joining "from" and "to" */
-    void rmEdge(Node<T>* from, Node<T>* to);
+    /* return length of successor list for node n */
+    template <typename T>
+    int Node<T>::outDegree()
+    {
+        int deg = 0;
+        return this->succs.size();
+    }
 
-    /* Tell if there is an edge from "from" to "to" */
-    bool goesTo(Node<T>* from, Node<T>* n);
-};
+    template <typename T>
+    std::map<int, Node<T> *> *Graph<T>::nodes()
+    {
+        return &this->mynodes;
+    }
 
-template <typename T>
-T Node<T>::nodeInfo() {
-    return this->info;
-}
+    template <typename T>
+    Node<T> *Graph<T>::addNode(T info)
+    {
+        Node<T> *node = new GRAPH::Node<T>(this->nodecount++, this, info, 0);
+        this->mynodes.insert({node->mykey, node});
+        return node;
+    }
 
-template <typename T>
-NodeSet* Node<T>::succ() {
-    return &this->succs;
-}
+    template <typename T>
+    void Graph<T>::rmNode(Node<T> *node)
+    {
+        assert(node->outDegree() == 0);
+        assert(node->inDegree() == 0);
+        node->mygraph->mynodes.erase(node->mykey);
+    }
 
-template <typename T>
-NodeSet* Node<T>::pred() {
-    return &this->preds;
-}
+    template <typename T>
+    void Graph<T>::addEdge(Node<T> *from, Node<T> *to)
+    {
+        assert(from);
+        assert(to);
+        assert(from->mygraph == to->mygraph);
 
-template <typename T>
-int Node<T>::nodeid() {
-    return this->mykey;
-}
+        if (goesTo(from, to))
+            return;
+        to->preds.insert(from->mykey);
+        from->succs.insert(to->mykey);
+    }
 
-template <typename T>
-int Node<T>::inDegree() {
-    int deg = 0;
-    return this->preds.size();
-}
+    template <typename T>
+    void Graph<T>::rmEdge(Node<T> *from, Node<T> *to)
+    {
+        assert(from && to);
+        to->preds.erase(to->preds.find(from->mykey));
+        from->succs.erase(from->succs.find(to->mykey));
+    }
 
-/* return length of successor list for node n */
-template <typename T>
-int Node<T>::outDegree() {
-    int deg = 0;
-    return this->succs.size();
-}
-
-template <typename T>
-std::map<int, Node<T>*>* Graph<T>::nodes() {
-    return &this->mynodes;
-}
-
-template <typename T>
-Node<T>* Graph<T>::addNode(T info) {
-    Node<T>* node = new GRAPH::Node<T>(this->nodecount++, this, info, 0);
-    this->mynodes.insert({node->mykey, node});
-    return node;
-}
-
-template <typename T>
-void Graph<T>::rmNode(Node<T>* node) {
-    assert(node->outDegree() == 0);
-    assert(node->inDegree() == 0);
-    node->mygraph->mynodes.erase(node->mykey);
-}
-
-template <typename T>
-void Graph<T>::addEdge(Node<T>* from, Node<T>* to) {
-    assert(from);
-    assert(to);
-    assert(from->mygraph == to->mygraph);
-
-    if (goesTo(from, to))
-        return;
-    to->preds.insert(from->mykey);
-    from->succs.insert(to->mykey);
-}
-
-template <typename T>
-void Graph<T>::rmEdge(Node<T>* from, Node<T>* to) {
-    assert(from && to);
-    to->preds.erase(to->preds.find(from->mykey));
-    from->succs.erase(from->succs.find(to->mykey));
-}
-
-template <typename T>
-bool Graph<T>::goesTo(Node<T>* from, Node<T>* n) {
-    return from->succs.count(n->mykey);
-}
-}  // namespace GRAPH
+    template <typename T>
+    bool Graph<T>::goesTo(Node<T> *from, Node<T> *n)
+    {
+        return from->succs.count(n->mykey);
+    }
+} // namespace GRAPH
